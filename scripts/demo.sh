26bf92a2ceb6265ea3ded9d66b0ce37fa6b9fc2a
Weinan Zhang <zwn1989@gmail.com>
2014-11-18 10:15:13 +0000

    Update demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -8,7 +8,12 @@ fi
 
 for campaign in $campaigns; do
     echo $campaign
-    #python ../python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
+    # run logistc regression
+    python ../python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
+    
+    # run real-time bidding test
     python ../python/rtb-test.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt $folder/$campaign/test.yzx.txt.lr.pred $resultsfolder/rtb.results.$campaign.tsv
+    
+    # check the best perf
     python ../python/check-best-perf.py $resultsfolder/rtb.results.$campaign.tsv
 done
